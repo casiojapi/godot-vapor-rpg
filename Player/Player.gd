98561aeef68c17b83718be2de0ac5d9557d1951c
viieja <msantoro@fi.uba.ar>
2020-10-09 12:42:10 -0300

    walking and idle animation  working - fix up/down

--- Player/Player.gd
+++ Player/Player.gd
@@ -3,7 +3,8 @@ extends KinematicBody2D
 var velocity = Vector2.ZERO
 
 onready var animation_player = $AnimationPlayer
-
+onready var animation_tree = $AnimationTree
+onready var animation_state = animation_tree.get("parameters/playback")
 const ACCELERATION = 1300
 const FRICTION = 1300
 const MAX_SPEED = 200
@@ -12,12 +13,16 @@ func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		animation_tree.set("parameters/Idle/blend_position", input_vector)
+		animation_tree.set("parameters/Run/blend_position", input_vector)
+		animation_state.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+		
 	else:
+		animation_state.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	velocity = move_and_slide(velocity)
