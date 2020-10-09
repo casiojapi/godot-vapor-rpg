6d8e81a88f57982583b55c9fd5fc19943dbdaf11
viieja <msantoro@fi.uba.ar>
2020-10-09 12:00:06 -0300

    player movement and collision working

---
+++ Player/Player.gd
@@ -0,0 +1,23 @@
+extends KinematicBody2D
+
+var velocity = Vector2.ZERO
+
+onready var animation_player = $AnimationPlayer
+
+const ACCELERATION = 1300
+const FRICTION = 1300
+const MAX_SPEED = 200
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	
+	velocity = move_and_slide(velocity)
