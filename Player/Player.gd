a692869f9778c35abd55f1d7cb8377d961a3b63e
viieja <msantoro@fi.uba.ar>
2020-10-10 17:54:53 -0300

    attack animation input working (animation tree too)

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,5 +1,12 @@
 extends KinematicBody2D
 
+enum {
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
+var state = MOVE
 var velocity = Vector2.ZERO
 
 onready var animation_player = $AnimationPlayer
@@ -9,7 +16,20 @@ const ACCELERATION = 1300
 const FRICTION = 1300
 const MAX_SPEED = 200
 
-func _physics_process(delta):
+func _ready():
+		animation_tree.active = true;
+
+func _process(delta):	#_physics_process if im using physics (position)
+	match state:
+		MOVE:
+			move_state(delta)
+		ROLL:
+			pass
+		ATTACK:
+			attack_state(delta)
+
+
+func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -18,6 +38,7 @@ func _physics_process(delta):
 	if input_vector != Vector2.ZERO:
 		animation_tree.set("parameters/Idle/blend_position", input_vector)
 		animation_tree.set("parameters/Run/blend_position", input_vector)
+		animation_tree.set("parameters/Attack/blend_position", input_vector)
 		animation_state.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 		
@@ -26,3 +47,15 @@ func _physics_process(delta):
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	velocity = move_and_slide(velocity)
+	
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
+
+
+
+func attack_state(delta):
+	velocity = Vector2.ZERO
+	animation_state.travel("Attack")
+	
+func attack_animation_finished():
+	state = MOVE
