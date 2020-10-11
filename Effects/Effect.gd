bfbb4e269b86ab32eb17ad9aa07785b065ccff4e
viieja <msantoro@fi.uba.ar>
2020-10-11 12:03:15 -0300

    enemies AI working

--- Effects/Effect.gd
+++ Effects/Effect.gd
@@ -1,7 +1,5 @@
 extends AnimatedSprite
 
-
-
 func _ready():
 	connect("animation_finished", self, "_on_animation_finished")
 	frame = 0
--- enemy/Bat.gd
+++ enemy/Bat.gd
@@ -1,10 +1,28 @@
 extends KinematicBody2D
+
+
 const EnemyDeathEffect = preload("res://Effects/EnemyDeathEffect.tscn")
-
-const KNOCK = 115
-var knockback = Vector2.ZERO	
-
 onready var stats = $Stats
+onready var player_detection_zone = $PlayerDetectionZone
+onready var sprite = $AnimatedSprite
+
+enum {
+	IDLE,
+	WANDER,
+	CHASE
+}
+
+
+export var ACCELERATION = 300
+export var MAX_SPEED = 50
+export var FRICTION = 200
+export var KNOCK = 115
+
+
+var velocity = Vector2.ZERO
+var knockback = Vector2.ZERO	
+var state = IDLE
+
 
 func _ready():
 	print(stats.max_health)
@@ -14,6 +32,26 @@ func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, 200 * delta)
 	knockback = move_and_slide(knockback)
 	
+	match state:
+		IDLE:
+			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+			seek_player()
+		WANDER:
+			pass
+		CHASE:
+			var player = player_detection_zone.player
+			if player != null:
+				var direction = (player.global_position - global_position).normalized()
+				velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+			else:
+				state = IDLE
+			sprite.flip_h = velocity.x < 0
+	velocity = move_and_slide(velocity)
+	
+	
+func seek_player():
+	if player_detection_zone.can_see_player():
+		state = CHASE
 	
 func _on_Hurtbox_area_entered(area):
 	stats.health -= area.damage
